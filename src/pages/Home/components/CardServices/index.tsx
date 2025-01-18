2e7591e6cab8d058b5b60f4230a23b8ba1aee11c
Windyyyyyyyyy <phonglagio17@gmail.com>
2025-01-18 14:59:29 +0700

    add animation

--- src/pages/Home/components/CardServices/index.tsx
+++ src/pages/Home/components/CardServices/index.tsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router';
 import type { CardServices } from '../../../../types';
+import { useEffect } from 'react';
+import initAOS from '../../../../utils/aos';
 
 const CardServices = ({ ...props }: CardServices) => {
+  useEffect(() => {
+    initAOS();
+  }, []);
   return (
-    <div className="w-full pt-7 md:w-1/2 md:px-[14px]">
+    <div className="w-full pt-7 md:w-1/2 md:px-[14px]" data-aos="zoom-in">
       <div className="relative h-full overflow-hidden rounded border-[0.1px] px-8 py-8">
         <div className="absolute -right-7 -top-9 h-24 w-24 rounded-[50%] bg-primary">
           <img src={props.icon} alt="service icon" className="absolute bottom-5 left-6" />
