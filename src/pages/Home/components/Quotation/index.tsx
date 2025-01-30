b018e15d963124fa4bf3504298c69efe69694f2d
Windyyyyyyyyy <phonglagio17@gmail.com>
2025-01-31 06:10:09 +0700

    update

--- src/pages/Home/components/Quotation/index.tsx
+++ src/pages/Home/components/Quotation/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import initAOS from '../../../../utils/aos';
+
 const Quotation = () => {
   const thValue = [
     'Logistics Company',
@@ -60,34 +63,40 @@ const Quotation = () => {
 
   const tdStyle = 'border px-1 py-1 text-center';
 
+  useEffect(() => {
+    initAOS();
+  }, []);
+
   return (
-    <div className="overflow-x-auto">
-      <table className="w-full table-auto overflow-scroll">
-        <thead>
-          <tr className="bg-gray-100">
-            {thValue.map((th, index) => (
-              <th key={index} className="px-2 py-2 text-center">
-                {th}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {trValue.map((tr, index) => (
-            <tr key={index} className="border-b border-gray-200">
-              <td className={`${tdStyle} w-48`}>{tr.logisticsCompany}</td>
-              <td className={tdStyle}>{tr.originDestination}</td>
-              <td className={`${tdStyle} w-20`}>{tr.containerSizeType}</td>
-              <td className={`${tdStyle} whitespace-nowrap`}>{tr.etd}</td>
-              <td className={tdStyle}>{tr.transitTime}</td>
-              <td className={tdStyle}>{tr.surcharges}</td>
-              <td className={tdStyle}>{tr.freightCost}</td>
-              <td className={tdStyle}>{tr.totalCost}</td>
-              <td className={`${tdStyle} whitespace-nowrap`}>{tr.validTill}</td>
+    <div data-aos="fade-up">
+      <div className="overflow-x-auto">
+        <table className="w-full table-auto overflow-scroll">
+          <thead>
+            <tr className="bg-gray-100">
+              {thValue.map((th, index) => (
+                <th key={index} className="px-2 py-2 text-center">
+                  {th}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {trValue.map((tr, index) => (
+              <tr key={index} className="border-b border-gray-200">
+                <td className={`${tdStyle} w-48`}>{tr.logisticsCompany}</td>
+                <td className={tdStyle}>{tr.originDestination}</td>
+                <td className={`${tdStyle} w-20`}>{tr.containerSizeType}</td>
+                <td className={`${tdStyle} whitespace-nowrap`}>{tr.etd}</td>
+                <td className={tdStyle}>{tr.transitTime}</td>
+                <td className={tdStyle}>{tr.surcharges}</td>
+                <td className={tdStyle}>{tr.freightCost}</td>
+                <td className={tdStyle}>{tr.totalCost}</td>
+                <td className={`${tdStyle} whitespace-nowrap`}>{tr.validTill}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
